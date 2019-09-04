d1353d6ccd50649560934d80404bfe9c26dfbd3f
hongfei <hongfei.tian@mobilemd.cn>
2019-09-04 20:25:16 +0800

    格式整理

--- routers/index.js
+++ routers/index.js
@@ -7,11 +7,11 @@ const Index = require('../controllers/index')
 router.prefix('/')
 
 router.get('/', (ctx, next)=> {
-    ctx.body = `
-        欢迎来到koa2 MC层~
-        路由 /mysql 请求数据库演示
-        <s>前端小白QQ群： 213787432   欢迎你的加入~
-    `
+    ctx.body = JSON.stringify({
+        '欢迎来到': 'koa2 MC层~',
+        '路由': '/mysql 请求数据库演示',
+        '前端小白QQ群': '213787432   欢迎你的加入~'
+    }, null, '\t')
 })
 router.get('/mysql', Index.findAllUserData)
 
