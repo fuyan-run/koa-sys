b4f5ea6e43bfa5c4b0b19e367347351061784191
fuyan-run <752781621@qq.com>
2019-09-20 11:33:27 +0800

    增加注释常用变量

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,12 @@
 // @author 啥都不会charles QQ: 752781621
-
+/*
+@author   对类的说明 标明开发该类模块的作者
+@version   对类的说明 标明该类模块的版本
+@see     对类、属性、方法的说明 参考转向，也就是相关主题
+@param    对方法的说明 对方法中某参数的说明
+@return   对方法的说明 对方法返回值的说明
+@exception  对方法的说明 对方法可能抛出的异常进行说明
+*/
 const Query = require('../pool/index')
 
 class Index {
